d8efa40d557171e56dc1ca787a39123b73ff7886
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-16 15:51:14 +0700

    Menambahkan rata-rata

--- Whilekelipatan21.java
+++ Whilekelipatan21.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Whilekelipatan21 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan, jumlah, counter;
+        int kelipatan, jumlah, counter, rataRata;
         jumlah = 0;
         counter = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
@@ -18,8 +18,10 @@ public class Whilekelipatan21 {
             }
             i++;
         }
+        rataRata = jumlah / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-Rata kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
         scan.close();
     }
 }
